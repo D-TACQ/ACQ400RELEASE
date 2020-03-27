29070b8c0f80a80a5dd9e2aea3f8190f3d27307e
Peter Milne <peter.milne@d-tacq.com>
2020-03-27 13:53:38 +0000

    update to new fpga*.img scheme 	modified:   scripts/make.release 	new file:   scripts/make_compatible_fpga_links.py 	modified:   tgt_scripts/update_release 	modified:   tgt_scripts/update_release_acq400

---
+++ scripts/make_compatible_fpga_links.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""
+make_compatible_fpga_links.py make softlinks for all compatible payload combos for a personality.
+
+
+ACQ2106_TOP_A5_ff_ff_ff_ff_ff -> ACQ2106_TOP_A5_A5_A5_A5_A5_A5
+ACQ2106_TOP_A5_A5_ff_ff_ff_ff -> ACQ2106_TOP_A5_A5_A5_A5_A5_A5
+ACQ2106_TOP_A5_A5_A5_ff_ff_ff -> ACQ2106_TOP_A5_A5_A5_A5_A5_A5
+ACQ2106_TOP_A5_A5_A5_A5_ff_ff -> ACQ2106_TOP_A5_A5_A5_A5_A5_A5
+ACQ2106_TOP_A5_A5_A5_A5_A5_ff -> ACQ2106_TOP_A5_A5_A5_A5_A5_A5
+
+
+"""
+
+import os
+
+FPGAD="./MNT/fpga.d"
+
+SITE1 = 2
+
+def get_nsites(fields):
+    carrier = fields[0]
+    if carrier == 'ACQ2106':
+        return 6
+    elif carrier == 'ACQ1001':
+        return 2
+    elif carrier.startswith("KMCU"):
+        return 2
+    else:
+        print("# WARNING: ignoring CARRIER {}".format(carrier))
+        return -1
+
+verbose = False
+
+files = os.listdir(FPGAD)
+
+print("#!/bin/bash")
+print("cd {}".format(FPGAD))
+
+for name in files:
+    #print(name)
+    fields = name.split('_')
+    nsites = get_nsites(fields)
+    if nsites > 0:
+        m1 = fields[SITE1]
+        ndup = 0
+        for f in fields[SITE1+1:]:
+            if verbose:
+                print("{} {} {}".format(fields[0], m1, f))
+            if f == m1:
+                ndup += 1
+            else:
+                break
+
+        for id in range(SITE1+1,SITE1+1+ndup):
+            fields2 = fields.copy()
+            for ff in range(id,SITE1+1+ndup):
+                fields2[ff] = 'ff'
+            print("ln -s {} {}".format(name, '_'.join(fields2)))
