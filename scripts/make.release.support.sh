178b80e34fe51b9839c5b752f3961e2e56629d89
Peter Milne <peter.milne@d-tacq.com>
2023-11-28 14:16:27 +0000

    add missing overlay acq2x06_enable_mgtAB.dtb split it to the common file make.release.support.sh so that at least the two flavours of make.release are not quite such a gemors.  On branch master 	modified:   scripts/make.release 	modified:   scripts/make.release.staffa3 	new file:   scripts/make.release.support.sh

---
+++ scripts/make.release.support.sh
@@ -0,0 +1,33 @@
+
+# source me. common support for make.release.*
+
+# subdirectories on target
+MNTDIRS="bin boot.d dtb.d fpga.d local packages packages.opt ko"
+
+
+# list of .dtb names to include
+ARCHDTB="\
+acq1001 acq1002 acq1002p acq1002r acq1002w acq400_uart0
+acq2006 acq2006b acq2006bEU
+acq2106d37 acq2106 acq2106sfp acq2106d37sfp acq210612
+acq1014
+kmcu kmcuz30 kmcuz30_bpe
+z7io
+acq2206
+acq2206sfp
+acq2106_quadcomms_overlay
+acq2106_hudp_overlay
+acq2106_1000X_overlay
+acq2106_sc_overlay
+acq400_usb_host_overlay
+acq400_usb_otg_overlay
+acq400_usb_peripheral_overlay
+acq2106_acq435elfx_sc_overlay
+acq400dsp1_overlay acq400dsp2_overlay
+z7io_cpld_overlay
+mgt482_sfp1_overlay
+mgt482_sfp2_overlay
+mgt482_sfp3_overlay
+mgt482_sfp4_overlay
+acq2x06_enable_mgtAB.dtb
+acq2x06_di460_overlay"
